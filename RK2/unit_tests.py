5d2232ff1098882d5889f8afd423210374df9e45
EvitaKB05 <149563886+EvitaKB05@users.noreply.github.com>
2024-12-18 00:19:28 +0300

    Update unit_tests.py

--- RK2/unit_tests.py
+++ RK2/unit_tests.py
@@ -1,24 +1,25 @@
 import unittest
 import main
-from operator import itemgetter
 
-class TestTasks(unittest.TestCase):
+class TestTaskFunctions(unittest.TestCase):
 
     def test_task1(self):
-        input_data = [('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust')]
-        expected_output = sorted(input_data, key=itemgetter(1))
-        self.assertEqual(main.task1(input_data), expected_output)
+        in_data = (('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust'))
+        correct_out = [('Vim', 1988, 'C++'), ('QT Creator', 1991, 'C++'), ('Visual Studio', 1997, 'C++'), ('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio Code', 2015, 'Rust')]
+        out = main.task1(in_data)
+        self.assertEqual(out, correct_out)
 
     def test_task2(self):
-        input_data = [('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust')]
-        expected_output = [('Rust', 1), ('Python', 2), ('C++', 3)] 
-        self.assertEqual(main.task2(input_data), expected_output)
+        in_data = (('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust'))
+        correct_out = [('Rust', 1), ('Python', 2), ('C++', 3)]
+        out = main.task2(in_data)
+        self.assertEqual(out, correct_out)
 
     def test_task3(self):
-        input_data = [('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust')]
-        expected_output = [('Pycharm', 'Python'), ('Visual Studio', 'C++'), ('QT Creator', 'C++'), ('Vim', 'C++')]
-        self.assertEqual(main.task3(input_data), expected_output)
-
+        in_data = (('Pycharm', 2013, 'Python'), ('CLion', 2014, 'Python'), ('Visual Studio', 1997, 'C++'), ('QT Creator', 1991, 'C++'), ('Vim', 1988, 'C++'), ('Visual Studio Code', 2015, 'Rust'))
+        correct_out = [('Pycharm', 'Python'), ('Visual Studio', 'C++'), ('QT Creator', 'C++'), ('Vim', 'C++')]
+        out = main.task3(in_data)
+        self.assertEqual(out, correct_out)
 
 
 if __name__ == '__main__':
